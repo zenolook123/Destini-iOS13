b18b636bab31751d2af040eff03f98231c9f7181
Zach Garrison <zgedina@yahoo.com>
2023-10-10 12:58:48 -0500

    jkjj

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -9,14 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var gameBrain = StoryBrain()
+ 
+    
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        if sender.tag == 0 {
+            print (gameBrain.storyNumber)
+            gameBrain.storyNumber = gameBrain.changeNumber1()
+            updateUI()
+        }
+        else {
+            print(gameBrain.storyNumber)
+            gameBrain.storyNumber = gameBrain.changeNumber2()
+                updateUI()
+            }
+        }
+      
+  
+    
+    func updateUI() {
+        storyLabel.text = gameBrain.getStory()
+        choice1Button.setTitle(gameBrain.getChoiceOne(), for: .normal)
+        choice2Button.setTitle(gameBrain.getChoiceTwo(), for: .normal)
+
+    }
+  
+ 
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        storyLabel.text = gameBrain.getStory()
+        choice1Button.setTitle(gameBrain.getChoiceOne(), for: .normal)
+        choice2Button.setTitle(gameBrain.getChoiceTwo(), for: .normal)
     }
 
 
